e14868c3debfd1e21248df23d76920fb2e0487ad
Sergio_Mir <voidtracer6@gmail.com>
2023-08-02 19:01:32 -0400

    ./scr/services/diaryServices.ts: ADD

--- scr/index.ts
+++ scr/index.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+
+import diaryRouter from './routes/diary'
+
 const app = express();
 
 app.use(express.json());
@@ -9,6 +12,8 @@ app.get('/ping', (_, res) => {
   console.log("someone pinged here! " + new Date().toLocaleDateString());
   res.send('pong');
 })
+
+app.use('/api/diaries', diaryRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
