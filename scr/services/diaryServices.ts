d572ed77e2150e941135b1c9fec7893fed036756
Sergio_Mir <voidtracer6@gmail.com>
2023-08-11 23:36:01 -0400

    ./scr/routes/diary.ts: MOD
    on the api/diaries/ endpoint will be returned all the entries without the comments

--- scr/services/diaryServices.ts
+++ scr/services/diaryServices.ts
@@ -5,6 +5,10 @@ const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
 
 export const getEntries = (): DiaryEntry[] => diaries
 
-export const getEntriesWithoutSensitiveInfo = (): EntriesWithoutSensitiveInfo[] => diaries
+export const getEntriesWithoutSensitiveInfo = (): EntriesWithoutSensitiveInfo[] => {
+  return diaries.map(({ id, date, weather, visibility }) => {
+    return { id, date, weather, visibility }
+  })
+}
 
 export const addEntry = (): undefined => {}
