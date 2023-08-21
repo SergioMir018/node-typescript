4a0f7932fa35d781fe73bdcc8a4c5f0bd180aa97
Sergio_Mir <voidtracer6@gmail.com>
2023-08-20 23:01:54 -0400

    ./scr/utils/newEntry.ts: MOD
    added some functions to parse part of the paramethers needed to create a new diary entry

--- scr/utils/newEntry.ts
+++ scr/utils/newEntry.ts
@@ -1,15 +1,36 @@
 import { NewDiaryEntry } from '../types'
 
-const parseComment = (commentFromRequest: any): String => {
-  if (typeof commentFromRequest !== 'string') {
+const parseComment = (commentFromRequest: any): string => {
+  if (!isString(commentFromRequest)) {
     throw new Error('Incorrect or missing comment')
   }
 
   return commentFromRequest
 }
 
+const parseDate = (dateFromRequest: any): string => {
+  if (!isString(dateFromRequest) || !isDate(dateFromRequest)) {
+    throw new Error('Incorrect or missing date')
+  }
+
+  return dateFromRequest
+}
+
+const isString = (string: any): boolean => {
+  return typeof string === 'string' || string instanceof String
+}
+
+const isDate = (date: any): boolean => {
+  return Boolean(Date.parse(date))
+}
+
 const toNewDiaryEntry = (object: any): NewDiaryEntry => {
-  const newEntry: NewDiaryEntry = {}
+  const newEntry: NewDiaryEntry = {
+    comment: parseComment(object.comment),
+    date: parseDate(object.date)
+  }
+
+  return newEntry
 }
 
 export default toNewDiaryEntry
